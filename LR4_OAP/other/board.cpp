769eac3ed4d52965e4584d5f98a696c779afdd25
eldak evrohimov <korzunov.matvey@yandex.ru>
2024-02-20 14:48:10 +0300

    minor changes

--- LR4_OAP/other/board.cpp
+++ LR4_OAP/other/board.cpp
@@ -8,21 +8,46 @@
 using json = nlohmann::json;
 
 void boardFunc() {
-    std::cout << "Ваша доска:" << std::endl;
-    std::cout << "   A B C D E F G H I J" << std::endl;
-    
-    // открытие файла
-    std::ifstream file("/Users/matthewkorzunov/Desktop/Учёба/Колледж/2 курс/2 семестр/ЛР 4 ОАП/LR4_OAP/LR4_OAP/other/data.json");
+    std::vector<std::string> rows = {
+        " 1 □ □ □ □ □ □ □ □ □ □",
+        " 2 □ □ □ □ □ □ □ □ □ □",
+        " 3 □ □ □ □ □ □ □ □ □ □",
+        " 4 □ □ □ □ □ □ □ □ □ □",
+        " 5 □ □ □ □ □ □ □ □ □ □",
+        " 6 □ □ □ □ □ □ □ □ □ □",
+        " 7 □ □ □ □ □ □ □ □ □ □",
+        " 8 □ □ □ □ □ □ □ □ □ □",
+        " 9 □ □ □ □ □ □ □ □ □ □",
+        "10 □ □ □ □ □ □ □ □ □ □"
+    };
 
-    // парс json из файла
-    json loadedData;
-    file >> loadedData;
+    json data;
 
+    // добавление данных
+    data["urBoard"] = "Ваша доска:";
+    data["letters"] = "   A B C D E F G H I J";
+    for (const auto& row : rows) {
+        data["rows"].push_back(row);
+    }
+
+    // записывание json в файл
+    std::ofstream file("/Users/matthewkorzunov/Desktop/Учёба/Колледж/2 курс/2 семестр/ЛР 4 ОАП/LR4_OAP/LR4_OAP/other/data.json");
+    file << std::setw(4) << data << std::endl;
     file.close();
 
+    // парс json из файла
+    std::ifstream readFile("/Users/matthewkorzunov/Desktop/Учёба/Колледж/2 курс/2 семестр/ЛР 4 ОАП/LR4_OAP/LR4_OAP/other/data.json");
+    json loadedData;
+    readFile >> loadedData;
+    readFile.close();
+
+//    std::string urBoard = loadedData["urBoard"];
+//    std::string letters = loadedData["letters"];
+    
     // вывод данных
-    for (auto& row : loadedData) {
-        std::string content = row["content"];
-        std::cout << content << std::endl;
+    std::cout << loadedData["urBoard"].get<std::string>() << std::endl;
+    std::cout << loadedData["letters"].get<std::string>() << std::endl;
+    for (const auto& row : loadedData["rows"]) {
+        std::cout << row.get<std::string>() << std::endl;
     }
 }
